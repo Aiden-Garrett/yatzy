60faa8a600b1dbc971b26cb1786e6b1d63df8d76
aiden <69060711+Aiden-Garrett@users.noreply.github.com>
2024-07-12 16:52:37 -0400

    added button to roll dice

--- public/index.php
+++ public/index.php
@@ -1,22 +1,39 @@
 <?php
+
+
 require_once('_config.php');
+?>
 
-use Yatzy\Dice;
-use Yatzy\YatzyGame;
+<div id="output">--</div>
+<button id="version">Version</button>
 
-//$d = new Dice();
+<script>
+    const output = document.getElementById("output");
+    const version = document.getElementById("version");
+    version.onclick = function (e) {
+        output.innerHTML = "Look up version clicked";
+    }
+</script>
 
-//for ($i=1; $i<=5; $i++) {
-//    echo "ROLL {$i}: {$d->roll()}<br>";
-//}
-
-// testing the game
-$game = new YatzyGame();
-echo $game;
-$game->roll();
-echo $game;
-// note the function takes the index of the dice to toggle (different from the visual dice num).
-$game->toggleKeeper(1);
-$game->toggleKeeper(2);
-$game->roll();
-echo $game;
+<!--// prev lab-->
+<!--//require_once('_config.php');-->
+<!--//-->
+<!--//use Yatzy\Dice;-->
+<!--//use Yatzy\YatzyGame;-->
+<!--//-->
+<!--////$d = new Dice();-->
+<!--//-->
+<!--////for ($i=1; $i<=5; $i++) {-->
+<!--////    echo "ROLL {$i}: {$d->roll()}<br>";-->
+<!--////}-->
+<!--//-->
+<!--//// testing the game-->
+<!--//$game = new YatzyGame();-->
+<!--//echo $game;-->
+<!--//$game->roll();-->
+<!--//echo $game;-->
+<!--//// note the function takes the index of the dice to toggle (different from the visual dice num).-->
+<!--//$game->toggleKeeper(1);-->
+<!--//$game->toggleKeeper(2);-->
+<!--//$game->roll();-->
+<!--//echo $game;-->
