a4cae2a6fdb23b6e15eb514e898dc712e4fbd011
aiden <69060711+Aiden-Garrett@users.noreply.github.com>
2024-07-13 13:09:33 -0400

    converted rest of yatzy_engine methods into php

--- app/models/YatzyEngine.php
+++ app/models/YatzyEngine.php
@@ -115,118 +115,120 @@ function calculateTwoPairs($diceState)
 //// console.log(calculateTwoPairs([1,1,2,2,5]));
 //// console.log(calculateTwoPairs([6,1,2,2,6]));
 //
-//function calculateThreeOfAKind($diceState) {
-//    // this works because there is 5 dice
-//    $numCount = new Map();
-//    for ($i = 0; $i < count($diceState); $i++) {
-//        let count = numCount.has($diceState[$i]) ? numCount.get($diceState[$i]) + 1 : 1;
-//
-//        // if number appears three times
-//        if (count === 3) {
-//            return $diceState[$i] * 3;
-//        }
-//        numCount.set($diceState[$i], count);
-//    }
-//    return 0;
-//}
 
-//function calculateFourOfAKind($diceState) {
-//    // this works because there is 5 dice
-//    let numCount = new Map();
-//    for ($i = 0; $i < count($diceState); $i++) {
-//        let count = numCount.has($diceState[$i]) ? numCount.get($diceState[$i]) + 1 : 1;
-//
-//        if (count === 4) {
-//            return $diceState[$i] * 4;
-//        }
-//        numCount.set($diceState[$i], count);
-//    }
-//    return 0;
-//}
-//
-//function calculateSmallStraight($diceState) {
-//    let nums = new Set($diceState);
-//    // check if the set has numbers 1-5
-//    for ($i = 1; $i <= 5; $i++) {
-//        if (!nums.has($i)) {
-//            return 0;
-//        }
-//    }
-//    return 15;
-//}
-//
-//function calculateLargeStraight($diceState) {
-//    let nums = new Set($diceState);
-//    // check if the set has numbers 2-6
-//    for ($i = 2; $i <= 6; $i++) {
-//        if (!nums.has($i)) {
-//            return 0;
-//        }
-//    }
-//    return 20;
-//}
-//
-//function calculateFullHouse($diceState) {
-//    // full house = three of a kind + a two of a kind that are distinct
-//    let numCount = new Map();
-//
-//    // get count of each number
-//    for ($i = 0; $i < count($diceState); $i++) {
-//        let count = numCount.has($diceState[$i]) ? numCount.get($diceState[$i]) + 1 : 1;
-//        numCount.set($diceState[$i], count);
-//    }
-//
-//    $score = 0;
-//    for (let [num, count] of numCount) {
-//        if (count !== 2 && count!== 3) {
-//            return 0;
-//        }
-//        $score += num * count;
-//    }
-//
-//    return $score;
-//}
-//
+function calculateThreeOfAKind($diceState): int
+{
+    $numCount = array_count_values($diceState);
+    // sort by key in descending order
+    krsort($numCount);
+
+    foreach ($numCount as $value => $count) {
+        if ($count >= 3) {
+            return $value * 3;
+        }
+    }
+
+    return 0;
+}
+
+//echo calculateThreeOfAKind([6, 6, 2, 2, 6]);
+
+function calculateFourOfAKind($diceState): int
+{
+    $numCount = array_count_values($diceState);
+    krsort($numCount);
+
+    foreach ($numCount as $value => $count) {
+        if ($count >= 4) {
+            return $value * 4;
+        }
+    }
+
+    return 0;
+}
+
+function calculateSmallStraight($diceState): int
+{
+    // check if the set has numbers 1-5
+    for ($i = 1; $i <= 5; $i++) {
+        if (!in_array($i, $diceState)) {
+            return 0;
+        }
+    }
+    return 15;
+}
+
+echo calculateSmallStraight([1, 2, 3, 4, 6, 5]);
+
+
+function calculateLargeStraight($diceState): int
+{
+    // check if the set has numbers 2-6
+    for ($i = 2; $i <= 6; $i++) {
+        if (!in_array($i, $diceState)) {
+            return 0;
+        }
+    }
+    return 20;
+}
+
+function calculateFullHouse($diceState)
+{
+    // full house = three of a kind + a two of a kind that are distinct
+
+    $numCount = array_count_values($diceState);
+    $score = 0;
+
+    foreach ($numCount as $value => $count) {
+        // if count is not 2 or 3 then cannot be full house
+        if ($count !== 2 && $count !== 3) {
+            return 0;
+        }
+        $score += $value * $count;
+    }
+
+    return $score;
+}
+
 //// console.log(calculateFullHouse([1,2,3,4,5]))
 //// console.log(calculateFullHouse([1,1,3,3,3]))
 //// console.log(calculateFullHouse([6,5,5,6,5]))
-//
-//function calculateChance($diceState) {
-//    $score = 0;
-//    for ($i = 0; $i < count($diceState); $i++) {
-//        $score += $diceState[$i];
-//    }
-//    return $score;
-//}
-//
-//function calculateYatzy(diceStates) {
-//    for ($i = 1; $i < diceStates.length; $i++) {
-//        if (diceStates[$i] !== diceStates[$i - 1]) {
-//            return 0;
-//        }
-//    }
-//    return 50;
-//}
-//
-//
-//function calculateUpperSum(gameState) {
-//    // null = 0 in js so this works
-//    return gameState['ones'] + gameState['twos'] + gameState['threes'] + gameState['fours'] + gameState['fives'] + gameState['sixes'];
-//}
-//
-//function calculateBonus(gameState) {
-//    // if upper section $score is 63+ -> give bonus
-//    return calculateUpperSum(gameState) >= 63 ? 50 : 0
-//}
-//
-//function calculateTotal(gameState) {
-//    let total = 0;
-//
-//    for (const value of Object.values(gameState)) {
-//        total += value;
-//    }
-//
-//    total += calculateBonus(gameState);
-//
-//    return total;
-//}
+
+function calculateChance($diceState) {
+    $score = 0;
+    for ($i = 0; $i < count($diceState); $i++) {
+        $score += $diceState[$i];
+    }
+    return $score;
+}
+
+function calculateYatzy($diceStates): int {
+    for ($i = 1; $i < count($diceStates); $i++) {
+        if ($diceStates[$i] !== $diceStates[$i - 1]) {
+            return 0;
+        }
+    }
+    return 50;
+}
+
+function calculateUpperSum($gameState) {
+    // null = 0 in js so this works
+    return $gameState['ones'] + $gameState['twos'] + $gameState['threes'] + $gameState['fours'] + $gameState['fives'] + $gameState['sixes'];
+}
+
+function calculateBonus($gameState): int {
+    // if upper section $score is 63+ -> give bonus
+    return calculateUpperSum($gameState) >= 63 ? 50 : 0;
+}
+
+function calculateTotal($gameState) {
+    $total = 0;
+
+    forEach ($gameState as $method => $score) {
+        $total += $score;
+    }
+
+    $total += calculateBonus($gameState);
+
+    return $total;
+}
